cb277d35b672965251f285f6fcab19e8cd7dce3e
fuGGet321 <137284031+fuGGet321@users.noreply.github.com>
2024-04-14 10:41:53 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -53,20 +53,20 @@ for g in enumerate(golfers):
 
 
 # # Get leaderboard from list type back in df type
-df_leaderboard = pd.DataFrame(leaderboard)
-headers = ["Player","Score", "Thru", "R1", "R2", "R3"]
-df_leaderboard.columns = headers
+# df_leaderboard = pd.DataFrame(leaderboard)
+# headers = ["Player","Score", "Thru", "R1", "R2", "R3"]
+# df_leaderboard.columns = headers
 
-# # Add Round Score placeholders
-# df_leaderboard.loc[:,['R1','R2','R3','R4']] = 0
+# # # Add Round Score placeholders
+# # df_leaderboard.loc[:,['R1','R2','R3','R4']] = 0
 
-df_leaderboard.reset_index(drop=True)
+# df_leaderboard.reset_index(drop=True)
 
-# # Change E to 0 so whole column can be changed to integers
-df_leaderboard.loc[df_leaderboard["Score"] == 'E', "Score"] = 0
+# # # Change E to 0 so whole column can be changed to integers
+# df_leaderboard.loc[df_leaderboard["Score"] == 'E', "Score"] = 0
 
-df_leaderboard.Score = df_leaderboard.Score.astype(int)
-df_leaderboard = df_leaderboard.sort_values("Score")
+# df_leaderboard.Score = df_leaderboard.Score.astype(int)
+# df_leaderboard = df_leaderboard.sort_values("Score")
 
 
 # %%
